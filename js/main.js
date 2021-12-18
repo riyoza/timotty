a322f0c88749bdb40dd825eb551e3debce2647cd
Timotty Aditya Riyoza <riyoza@outlook.com>
2021-12-18 22:35:38 +0700

    rework maintenis

---
+++ js/main.js
@@ -0,0 +1,21 @@
+// Set the Access Token
+var accessToken = "ec320d26700dc7631ee7e757a28ae367d15ca2d11601646a9d2c1874bc683f59"
+
+// Call Dribble v2 API
+$.ajax({
+    url: 'https://api.dribbble.com/v2/user/shots?access_token='+accessToken,
+    dataType: 'json',
+    type: 'GET',
+    success: function(data) {  
+      if (data.length > 0) { 
+        $.each(data.reverse(), function(i, val) {                
+          $('#dribbbleShots').prepend(
+            '<li class="shot" target="_blank" href="'+ val.html_url +'" title="' + val.title + '"><div class="title">' + val.title + '</div><img src="'+ val.images.hidpi +'"/></li>'
+            )
+        })
+      }
+      else {
+        $('#dribbbleShots').append('<p>Sorry, No shots yet</p>');
+      }
+    }
+});
